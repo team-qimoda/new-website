9bd2af470acdbd9a0237b1ac67120f10c01cc9a1
Sidney Ramos <sidney.ramos95@gmail.com>
2021-05-28 21:48:24 +1000

    Added custom document.js to fix styled-comps FOUC

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Head from "next/head";
 import Header from "components/Header";
 import Footer from "components/Footer";
@@ -13,17 +12,13 @@ const Main = styled.main`
 `;
 
 const Layout = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setLoading(false);
-  }, []);
   return (
     <div>
       <Head>
         <title>Qimoda - Great things in small packages</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {!loading && <Header />}
+      <Header />
 
       <Main>{children}</Main>
 
