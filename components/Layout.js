1e9c6cbc7cb2446e4d4b10dcd30d86ac3413e8fe
Sidney Ramos <sidney.ramos95@gmail.com>
2021-05-28 21:35:47 +1000

    added setLoading on Header

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import styled from "styled-components";
 
 const Main = styled.main`
-  // padding-bottom: 5rem;
   flex: 1;
   display: flex;
   flex-direction: column;
@@ -13,13 +13,17 @@ const Main = styled.main`
 `;
 
 const Layout = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
     <div>
       <Head>
         <title>Qimoda - Great things in small packages</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      {!loading && <Header />}
 
       <Main>{children}</Main>
 
